e489fbe09abf78a9f63e8e5513b3610958215e34
Tomas Ferenc <tomas.ferenc@notino.com>
2022-11-22 00:23:48 +0100

    Home page edited

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,9 @@
-import React from 'react';
-import { API } from '../utils/API';
 import { Container } from 'react-bootstrap';
 
 const Home = () => {
-  const [title, setTitle] = React.useState('');
-
-  React.useEffect(() => {
-    const getHomeTitle = async () => {
-      const response = await API.get('');
-      setTitle(response);
-    };
-    getHomeTitle();
-  }, []);
-
   return (
     <Container className='text-center mt-5'>
-      <h1>{title}</h1>
+      <h1>Welcome to home page</h1>
     </Container>
   );
 };

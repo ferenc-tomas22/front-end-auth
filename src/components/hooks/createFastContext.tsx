c6c3ca5e1b2745b75518a0d631630e627ab1339f
Tomas Ferenc <tomas.ferenc@notino.com>
2022-12-22 14:34:30 +0100

    few changes in fast context

--- src/components/hooks/createFastContext.tsx
+++ src/components/hooks/createFastContext.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function createFastContext<Store>(initialState: Store) {
-  function useStoreData(): {
+export function createFastContext<Store>(initialState: Store) {
+  const useStoreData = (): {
     get: () => Store;
     set: (value: Partial<Store>) => void;
     subscribe: (callback: () => void) => () => void;
-  } {
+  } => {
     const store = React.useRef(initialState);
 
     const get = React.useCallback(() => store.current, []);
@@ -27,24 +27,22 @@ export default function createFastContext<Store>(initialState: Store) {
       set,
       subscribe,
     };
-  }
+  };
 
   type UseStoreDataReturnType = ReturnType<typeof useStoreData>;
 
   const StoreContext = React.createContext<UseStoreDataReturnType | null>(null);
 
-  function Provider({ children }: { children: React.ReactNode }) {
+  const Provider = ({ children }: { children: React.ReactNode }) => {
     return <StoreContext.Provider value={useStoreData()}>{children}</StoreContext.Provider>;
-  }
+  };
 
   function useStore<SelectorOutput>(
     selector: (store: Store) => SelectorOutput
   ): [SelectorOutput, (value: Partial<Store>) => void] {
     const store = React.useContext(StoreContext);
 
-    if (!store) {
-      throw new Error('Store not found');
-    }
+    if (!store) throw new Error('Store not found');
 
     const state = React.useSyncExternalStore(store.subscribe, () => selector(store.get()));
 
